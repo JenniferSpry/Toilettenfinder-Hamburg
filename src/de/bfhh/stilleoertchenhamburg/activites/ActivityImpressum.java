aab81ba99ad7ad6a9dbd2485b7fab3b0d4faed02
JenniferSpry <jennifer.spry@web.de>
2014-10-18 22:06:06 +0200

    playing around with layout stuff

--- src/de/bfhh/stilleoertchenhamburg/activites/ActivityImpressum.java
+++ src/de/bfhh/stilleoertchenhamburg/activites/ActivityImpressum.java
@@ -1,8 +1,9 @@
 package de.bfhh.stilleoertchenhamburg.activites;
 
 import de.bfhh.stilleoertchenhamburg.R;
-import de.bfhh.stilleoertchenhamburg.R.layout;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
 public class ActivityImpressum extends ActivityMenuBase {
 
@@ -10,6 +11,9 @@ public class ActivityImpressum extends ActivityMenuBase {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.impressum);
+		
+		TextView t2 = (TextView) findViewById(R.id.impressum_link);
+	    t2.setMovementMethod(LinkMovementMethod.getInstance());
 	}
 
 }
